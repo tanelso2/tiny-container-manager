2ff7fe4acdf73b288599b3b1379c6e273e868282
Thomas Nelson <tanelso2@gmail.com>
2021-04-29 12:23:27 -0700

    More progress

--- src/tiny_container_manager.nim
+++ src/tiny_container_manager.nim
@@ -1,4 +1,4 @@
-import yaml.serialization, streams
+#import yaml.serialization, streams
 import 
   strformat,
   os,
@@ -22,8 +22,29 @@ proc simpleExec(command: string): string = command.split.runInShell
 
 proc isRunning(target: Container): bool =
   let containers = "docker container ls".simpleExec
-  return containers.contains(target.name)
+  for line in containers.split("\n")[1..^1]:
+    if line.contains(target.name):
+      return true
+  return false
+
+proc createContainer(target: Container) =
+  # TODO: check if running
+  let portArgs = fmt"-p {target.containerPort}"
+  let cmd = fmt"docker run --name {target.name} -d {portArgs} {target.image}"
+  echo cmd
+  echo cmd.simpleExec()
+
 
 when isMainModule:
   echo "hey hey hey"
+  let name = "k8s_rules-configmap-reloader_prometheus-prometheus-operator-prometheus-0"
+  let image = "nginx:latest"
+  let containerPort = 8080
+  let host = "thomasnelson.me"
+  let c = Container(name: name, image: image, containerPort: containerPort, host: host)
+  echo fmt"{c.name} is running? {c.isRunning}"
+  let c2 = Container(name: "tnelson-personal-website", image: image, containerPort: containerPort, host: host)
+  echo fmt"{c2.name} is running? {c2.isRunning}"
+  if not c2.isRunning:
+    c2.createContainer
 
