52e99914ac5799707c2cf69847be53adeea8c600
Thomas Nelson <tanelso2@gmail.com>
2022-07-28 14:36:12 -0500

    Improved install

--- deployment/install.sh
+++ deployment/install.sh
@@ -10,6 +10,18 @@ apt-get update
 apt-get install -y docker.io \
                    libssl-dev
 
+DOCKER_CREDS_GCR_RELEASE="https://github.com/GoogleCloudPlatform/docker-credential-gcr/releases/download/v2.1.5/docker-credential-gcr_linux_amd64-2.1.5.tar.gz"
+T=$(mktemp -d)
+cd $T
+wget $DOCKER_CREDS_GCR_RELEASE
+tar -zxf docker-credentials*.tar.gz
+cp docker-credentials-gcr "/usr/local/bin"
+
+docker-credentials-gcr configure-docker
+docker-credentials-gcr gcr-login
+
+cd $TCM_DIR
+
 SERVICE=tiny-container-manager.service
 SERVICE_FILE="$TCM_DIR/deployment/$SERVICE"
 
