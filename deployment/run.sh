aeea725182e8b49dd30c71f93fdc6d3f2bfe02fe
Thomas Nelson <tanelso2@gmail.com>
2023-02-28 16:33:35 -0600

    Run under valgrind massif

--- deployment/run.sh
+++ deployment/run.sh
@@ -3,9 +3,17 @@
 export PATH=/root/.nimble/bin:$PATH
 nimble choosenim
 nimble build
+## Run normally
+# ./tiny_container_manager
+## Memcheck
+# valgrind \
+#   --leak-check=full \
+#   --show-leak-kinds=all \
+#   --num-callers=16 \
+#   ./tiny_container_manager -d \
+#   >> valgrind-output 2>&1
+## Massif
 valgrind \
-  --leak-check=full \
-  --show-leak-kinds=all \
-  --num-callers=16 \
-  ./tiny_container_manager -d \
-  >> valgrind-output 2>&1
+    --tool=massif \
+    ./tiny_container_manager -d \
+    >> valgrind-output 2>&1
