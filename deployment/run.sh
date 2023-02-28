8926eab364378feaa7d28d42dc83ff37dc92f7ff
Thomas Nelson <tanelso2@gmail.com>
2023-02-27 19:00:01 -0600

    More in-depth valgrind calls

--- deployment/run.sh
+++ deployment/run.sh
@@ -3,4 +3,9 @@
 export PATH=/root/.nimble/bin:$PATH
 nimble choosenim
 nimble build
-valgrind --leak-check=full ./tiny_container_manager -d >> valgrind-output 2>&1
+valgrind \
+  --leak-check=full \
+  --show-leak-kinds=all \
+  --num-callers=16 \
+  ./tiny_container_manager -d \
+  >> valgrind-output 2>&1
